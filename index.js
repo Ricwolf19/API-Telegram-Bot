4cd3539a3bb653caad33d1f6dc15bed72bc1e42b
Ricardo Tapia <108904323+Ricwolf19@users.noreply.github.com>
2023-10-13 00:39:45 -0600

    Update index.js HIDE TOKEN

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '6509530725:AAGqDvkgCQ_WYaOinZD6ZLaBQ14jmvKNE1s'; //Token de el bot
+const token = ''; //Token de el bot
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', msg => {
